bf546f7054f7e811fb83b4cafd0e85edc0aeeb25
basixrox <basixrox@gmail.com>
2016-11-04 15:03:21 +0100

    initial commit of work in progress

---
+++ Sudoku/SudokuGrid.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sudoku
+{
+    class SudokuGrid
+    {
+        private int[,] data;
+
+        #region Constructors
+        public SudokuGrid()
+        {
+            new SudokuGrid(new int[9, 9]);
+        }
+
+        public SudokuGrid(int[,] myData)
+        {
+            this.data = myData;
+            if (!isValid())
+            {
+                this.data = new int[9, 9];
+            }
+        }
+        #endregion
+
+        #region Contains functions
+        /// <summary>
+        /// Checks if the given row already contains the given number
+        /// </summary>
+        /// <param name="rowNumber">the row to check</param>
+        /// <param name="contains">the number to look for</param>
+        /// <returns>true if number was found, false otherwise</returns>
+        public Boolean rowContains(int rowNumber, int contains)
+        {
+            for (int col=0; col<9; col++)
+            {
+                if (data[rowNumber,col] == contains)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+       /// <summary>
+        /// Checks if the given column already contains the given number
+        /// </summary>
+        /// <param name="colNumber">the column to check</param>
+        /// <param name="contains">the number to look for</param>
+        /// <returns>true if number was found, false otherwise</returns>
+        public Boolean colContains(int colNumber, int contains)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                if (data[row, colNumber] == contains)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if the given square contains the given number
+        /// </summary>
+        /// <param name="square"> ID of the square (0 based, first row 0-2,
+        /// second row 3-5, third row 6-8)</param>
+        /// <param name="contains">the number to look for</param>
+        /// <returns>true, if number was found, false otherwise</returns>
+        public Boolean squareContains(int square, int contains)
+        {
+            int row = (square / 3) * 3;
+
+            for (int i = 0; i < 2; i++)
+            {
+                int col = (square % 3) * 3;
+                for (int j = 0; j < 2; j++)
+                {
+                    if(data[row,col] == contains)
+                    {
+                        return true;
+                    }
+                    col++;
+                }
+                row++;
+            }
+            return false;
+        }
+        #endregion
+
+        #region getter and setter
+        public int[,] getData()
+        {
+            return data;
+        }
+
+        public Boolean setData(int[,] newData)
+        {
+            int[,] oldData = data;
+            data = newData;
+            if (!isValid())
+            {
+                data = oldData;
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+        #region Others
+        /// <summary>
+        /// Checks if the saved grid is valid
+        /// </summary>
+        /// <returns>true, if valid, false otherwise</returns>
+        public Boolean isValid()
+        {
+
+            #region check each row and column
+            for (int i = 0; i < 9; i++)
+            {
+                List<int> colValues = new List<int>();
+                List<int> rowValues = new List<int>();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    int row = data[i, j];
+                    int col = data[j, i];
+
+                    if (col != 0 && colValues.Contains(col))
+                    {
+                        MessageBox.Show(String.Format("Row {0} (column {1}) contains a duplicate number ({2}) for that column!", j, i, col));
+                        return false;
+                    }
+
+                    if (row != 0 && rowValues.Contains(row))
+                    {
+                        MessageBox.Show(String.Format("Row {0} (column {1}) contains a duplicate number ({2}) for that row!", i, j, row));
+                        return false;
+                    }
+
+                    colValues.Add(col);
+                    rowValues.Add(row);
+                }
+            }
+            #endregion
+
+            #region check each square
+            for (int square = 0; square < 9; square++)
+            {
+                List<int> squareValues = new List<int>();
+                int rowStart = (square / 3) * 3;
+                int rowMax = rowStart + 2;
+                int colStart = (square % 3) * 3;
+                int colMax = colStart + 2;
+
+                for (int row = rowStart; row < rowMax; row++)
+                {
+                    for (int col = colStart; col < colMax; col++)
+                    {
+                        int current = data[row, col];
+
+                        if (current != 0 && squareValues.Contains(current))
+                        {
+                            MessageBox.Show(String.Format("Square {0} contains a duplicate {1}", square, current));
+                            return false;
+                        }
+                        squareValues.Add(current);
+                    }
+                }
+            }
+            #endregion
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ Sudoku/View.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SudokuGrid sudokuGrid;
+        Solver solver;
+
+        #region Initialization
+        public MainWindow()
+        {
+            InitializeComponent();
+            clearAllFields();
+            initializeGlobals();
+        }
+
+        /// <summary>
+        /// remove all values from all text inputs
+        /// </summary>
+        private void clearAllFields()
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox current = (TextBox)this.FindName("txt" + row.ToString() + col.ToString());
+                    current.Text = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// resets all global variables
+        /// </summary>
+        private void initializeGlobals()
+        {
+            sudokuGrid = new SudokuGrid();
+            solver = new Solver();
+        }
+        #endregion
+
+        #region Buttons
+        /// <summary>
+        /// exits the program
+        /// </summary>
+        private void exitButton(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// will be used to solve the entered sudoku puzzle
+        /// </summary>
+        private void solveButton(object sender, RoutedEventArgs e)
+        {
+            sudokuGrid.setData(createGridFromFields());
+        }
+
+        /// <summary>
+        /// will be used to generate a new sudoku puzzle
+        /// </summary>
+        private void generateButton(object sender, RoutedEventArgs e)
+        {
+            //TODO
+            int[,] test = new int[9, 9];
+            fillFieldsFromGrid(test);
+        }
+        #endregion
+
+        #region Event Handlers
+        /// <summary>
+        /// Ask user to confirm before closing the program
+        /// </summary>
+        private void onClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult exit = MessageBox.Show("Are you sure, you want to exit this application? All your changes will be lost.", "Please confirm", MessageBoxButton.YesNoCancel);
+
+            if (exit != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Make sure, that only numbers from 1-9 can be entered
+        /// </summary>
+        private void updatedGrid(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^[^1-9].{0}");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        /// <summary>
+        /// move to adjacent input fields with the arrow keys
+        /// </summary>
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            TextBox txtBox = (TextBox)sender;
+            int row = int.Parse(txtBox.Name.Substring(3, 1));
+            int col = int.Parse(txtBox.Name.Substring(4, 1));
+
+            switch (e.Key)
+            {
+                case Key.Down:
+                    e.Handled = true;
+                    row++;
+                    break;
+                case Key.Up:
+                    e.Handled = true;
+                    row--;
+                    break;
+                case Key.Left:
+                    e.Handled = true;
+                    col--;
+                    break;
+                case Key.Right:
+                    e.Handled = true;
+                    col++;
+                    break;
+            }
+
+            if (row < 0 || row > 8 || col < 0 || col > 8)
+            {
+                return;
+            }
+
+            txtBox = (TextBox)this.FindName("txt" + row.ToString() + col.ToString());
+            txtBox.Focus();
+        }
+        #endregion
+
+        #region Helpers
+        /// <summary>
+        /// Extracts all numbers from the texboxes in the view and saves
+        /// them in an array
+        /// </summary>
+        private int[,] createGridFromFields()
+        {
+            int[,] newGrid = new int[9, 9];
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0;col <9; col++)
+                {
+                    TextBox txtBox = (TextBox)this.FindName("txt" + row.ToString() + col.ToString());
+                    if(txtBox.Text != "")
+                    {
+                        newGrid[row, col] = int.Parse(txtBox.Text);
+                    }
+                }
+            }
+            return newGrid;
+        }
+
+        /// <summary>
+        /// publishes an int array [,] to the text boxes of the View
+        /// </summary>
+        /// <param name="grid">the array to show</param>
+        private void fillFieldsFromGrid(int[,] grid)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox txtBox = (TextBox)this.FindName("txt" + row.ToString() + col.ToString());
+                    int number = grid[row, col];
+                    if (number != 0)
+                    {
+                        txtBox.Text = number.ToString();
+                        txtBox.IsReadOnly = true;
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
